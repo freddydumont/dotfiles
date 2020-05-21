bd3ebea2e7af0059d3f8367ce96c43d162ea60d9
Frederick Morin <frederickmorin18@gmail.com>
2020-05-21 11:18:41 -0400

    Update brew-cask

--- brew-cask.sh
+++ brew-cask.sh
@@ -7,50 +7,51 @@
 
 # Install native apps
 
-brew install caskroom/cask/brew-cask
-# brew tap caskroom/versions
+brew tap homebrew/cask-versions
 
 # daily
-brew cask install spectacle
-brew cask install dropbox
-brew cask install gyazo
-brew cask install 1password
-brew cask install rescuetime
-# brew cask install flux
+brew cask install anki
+brew cask install calibre
+brew cask install flux
+brew cask install notion
+brew cask install setapp
+brew cask install slack
+brew cask install windscribe
 
 # dev
 brew cask install iterm2
 brew cask install sublime-text
+brew cask install visual-studio-code
+brew cask install gitkraken
+brew cask install unity-hub
 brew cask install imagealpha
 brew cask install imageoptim
+brew cask install quicklook-json
+
+# mech
+brew cask install qmk-toolbox
 
 # fun
-brew cask install limechat
-# brew cask install miro-video-converter
-# brew cask install horndis               # usb tethering
+brew cask install discord
+brew cask install steam
+brew cask install telegram
 
 # browsers
-# brew cask install google-chrome-canary
-# brew cask install firefoxnightly
-# brew cask install webkit-nightly
-# brew cask install chromium
-# brew cask install torbrowser
+brew cask install brave-browser
+brew cask install firefox
+brew cask install firefox-developer-edition
 
 # less often
-brew cask install disk-inventory-x
-brew cask install screenflow
-brew cask install vlc
-brew cask install gpgtools
-brew cask install licecap
-# brew cask install utorrent
+brew cask install transmission
+brew cask install wireshark
 
-brew cask install spotify
-
-brew tap caskroom/fonts 
+# fonts
+brew tap homebrew/cask-fonts 
 brew cask install font-fira-code
+brew cask install font-iosevka
 
 
 # Not on cask but I want regardless.
 
-# File Multi Tool 5
-# Phosphor
+# Migrate all setapp apps
+# https://support.setapp.com/hc/en-us/articles/360008560299
